8218c1c641592ea3f204732d46f70e53c9412b95
Sudipa <sudipasarkar999@gmail.com>
2020-02-26 20:08:58 +0530

    lowest common factor in java

--- LCM.java
+++ LCM.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-public class GCD
+public class LCM_algo
 {
   public static void main(String args[])
   {
       Scanner sc = new Scanner(System.in);
-      int a=sc.nextInt();
-      int b=sc.nextInt();
-      int temp;
+      long a=sc.nextInt();
+      long b=sc.nextInt();
+      long temp;
+      long a1,b1;
+      a1=a;
+      b1=b;
       while(b!=0)
      {
             temp=a;
             a=b;
             b=temp%b;
         }
-        System.out.println(a);
+        System.out.println((a1*b1)/a);
     }
 }
